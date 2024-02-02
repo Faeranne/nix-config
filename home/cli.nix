38c02853425f94c6ef25d43ff83bbcbffee02f8f
Nina Morgan <nina@projecmtkaeit.com>
2024-02-02 04:41:35 -0500

    Add vimdiff, tmux, and correctly enable vim config

--- home/cli.nix
+++ home/cli.nix
@@ -31,6 +31,7 @@ in
       defaultEditor = true;
       viAlias = true;
       vimAlias = true;
+      vimdiffAlias = true;
       plugins = with pkgs.vimPlugins; [
         vim-fugitive
         tabular
@@ -51,6 +52,32 @@ in
         vimproc-vim
         (fromGitHub "4a93122ae2139a12e2a56f064d086c05160b6835" "segeljakt" "vim-silicon" "sha256-8pCHtApD/xXav2UBVOVhkaHg3YS4aNCZ73mog04bYuA=")
       ];
+      extraConfig = ''
+        set nocompatible
+        syntax on
+        set encoding=utf-8
+        set tabstop=2
+        set shiftwidth=2
+        set expandtab
+
+        let g:silicon = {
+        \ 'theme':  'gruvbox',
+        \ 'font':   'Hack',
+        \}
+        
+        autocmd StdinReadPre * let s:std_in=1
+        autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists("s:std_in") | exe 'NERDTree' argv()[0] | wincmd p | ene | endif
+        autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif
+        autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif
+        let g:javascript_plugin_jsdoc = 1
+        set cmdheight=2
+        set updatetime=300
+        set shortmess+=c
+        
+        set signcolumn=number
+        
+        autocmd BufNewFile,BufRead *.mts  set filetype=typescript
+      '';
     };
     atuin = {
       enable = true;
@@ -63,6 +90,12 @@ in
     broot = {
       enable = true;
       enableZshIntegration = true;
+    };
+    tmux = {
+      enable = true;
+      keyMode = "vi";
+      newSession = true;
+      tmuxinator.enable = true;
     };
     zsh = {
       enable = true;
