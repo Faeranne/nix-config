3ae4ff60530a1e06baab0f64c277e3343baf73d3
Nina Morgan <nina@projectmakeit.com>
2024-02-15 14:36:26 -0500

    add someadditional options for future use in homepage

--- services/homepage.nix
+++ services/homepage.nix
@@ -1,21 +1,88 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.custom.homepage;
+  inherit (lib) mkOption types;
+
 in
 {
   options.custom.homepage = {
-    enable = lib.mkOption {
+    enable = mkOption {
       default = false;
       description = "Enable homepage module";
-      type = lib.types.bool;
+      type = types.bool;
     };
-    local = lib.mkOption {
+    local = mkOption {
       description = "Container IP address.";
-      type = lib.types.str;
+      type = types.str;
     };
-    url = lib.mkOption {
+    url = mkOption {
       description = "Url for homepage.";
-      type = lib.types.str;
+      type = types.str;
+    };
+    settings = {
+      title = mkOption {
+        default = "Homepage";
+        description = "Text for the titlebar.";
+        type = types.str;
+      };
+      startUrl = mkOption {
+        default = "/";
+        description = "Base url for apps.";
+        type = types.str;
+      };
+      background = {
+        image = mkOption {
+          description = "Background image.";
+          type = types.str;
+        };
+        blur = mkOption {
+          description = "Background image blur.";
+          type = types.str;
+        };
+        saturate = mkOption {
+          description = "Background image saturate.";
+          type = types.str;
+        };
+        brightness = mkOption {
+          description = "Background image brightness.";
+          type = types.str;
+        };
+        opacity = mkOption {
+          description = "Background image opacity.";
+          type = types.str;
+        };
+      };
+      cardBlur = mkOption {
+        description = "Background image blur for cards.";
+        type = types.str;
+      };
+      favicon = mkOption {
+        description = "Change the favicon.";
+        type = types.str;
+      };
+      theme = mkOption {
+        description = "Hardset the theme. 'light' or 'dark'";
+        type = types.enum ["light" "dark"];
+      };
+      color = mkOption {
+        description = "Hardset the color palette. options include slate, gray, zinc, neutral, stone, amber, yellow, lime, green, emerald, teal, cyan, sky, blue, indigo, violet, purple, fuchsia, pink, rose, red, white";
+        type = types.enum ["slate" "gray" "zinc" "neutral" "stone" "amber" "yellow" "lime" "green" "emerald" "teal" "cyan" "sky" "blue" "indigo" "violet" "purple" "fuchsia" "pink" "rose" "red" "white"];
+      };
+      layout = mkOption {
+        description = "Setup the service and bookmarks layouts.";
+        type = types.attrsOf (types.submodule {
+          options = {
+            style = mkOption {
+              description = "Whether the section is a row or column.";
+              type = types.enum ["row" "column"];
+            };
+            columns = mkOption {
+              description = "How many columns a row takes up. from 1 to 5.";
+              type = types.ints.between 1 5;
+            };
+          };
+        });
+      };
     };
   };
   config = lib.mkIf cfg.enable {
