20e3e02db1a9be850f3cb68b6c2eaf35daaab721
Nina Morgan <nina@projectmakeit.com>
2024-02-18 11:24:00 -0500

    add git lg to the homemanager lg

--- home/git.nix
+++ home/git.nix
@@ -6,6 +6,9 @@
       diff-so-fancy.enable = true;
       userEmail = "nina@projectmakeit.com";
       userName = "Nina Morgan";
+      aliases = {
+        lg = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all -n 15";
+      };
     };
     git-cliff.enable = true;
     gitui.enable = true;
