a415898f5896a6926c8127e1b7e7f42b43ff6b82
Faer-Anne <nina@projectmakeit.com>
2024-02-18 11:47:31 -0500

    Replace git diff tool. add some extra git configs I wanna try.

--- home/git.nix
+++ home/git.nix
@@ -3,12 +3,31 @@
   programs = {
     git = {
       enable = true;
-      diff-so-fancy.enable = true;
       userEmail = "nina@projectmakeit.com";
       userName = "Faer-Anne";
       aliases = {
         lg = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all -n 15";
       };
+      difftastic = {
+        enable = true;
+        background = "dark";
+        display = "side-by-side-show-both";
+        color = "always";
+      };
+      extraConfig = {
+        diff.algorithm = "histogram";
+        help.autocorrect = "prompt";
+        init.defaultBranch = "main";
+        merge.conflictstyle = "zdiff3";
+        push.default = "current";
+        rebase.autostash = true;
+        rerere.enabled = true;
+        url."git@github.com:".pushInsteadOf = "https://github.com/";
+      };
+      ignores = [
+        ".*.swp"
+        "*~"
+      ];
     };
     git-cliff.enable = true;
     gitui.enable = true;
