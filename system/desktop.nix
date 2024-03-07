d48177c55c1c19d0068e408cafe040b5c8857182
Faer-Anne <nina@projectmakeit.com>
2024-03-07 06:08:39 +0000

    add some desktop settings, including background

--- system/desktop.nix
+++ system/desktop.nix
@@ -154,10 +154,45 @@ in
             "appindicatorsupport@rgcjonas.gmail.com"
             "gsconnect@andyholmes.github.io"
           ];
+          favorite-apps = [
+            "firefox.desktop"
+            "discord.desktop"
+            "org.gnome.Console.desktop"
+            "obsidian.desktop"
+            "org.gnome.Nautilus.desktop"
+          ];
         };
         "org/gnome/desktop/background" = {
-         # picture-uri = "file:///home/nina/.local/share/backgrounds/2024-03-07-05-42-22-nexus_labs_v3.png";
-         # picture-uri-dark = "file:///home/nina/.local/share/backgrounds/2024-03-07-05-42-22-nexus_labs_v3.png";
+         picture-uri = ("file://" + ../resources/background.png);
+         picture-uri-dark = ("file://" + ../resources/background.png);
+        };
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+          enable-hot-corners = false;
+        };
+        "org/gnome/desktop/wm/preferences" = {
+          workspaces-names = [
+            "Main"
+          ];
+        };
+        "org/gnome/mutter" = {
+          edge-tiling = true;
+          dynamic-workspaces = true;
+          workspaces-only-on-primary = true;
+        };
+        "org/gnome/settings-daemon/plugins/color" = {
+          night-light-enabled = true;
+          night-light-schedule-automatic = true;
+          night-light-temperature = 3700;
+        };
+        "org/gnome/settings-daemon/plugins/power" = {
+          sleep-inactive-ac-type = "nothing";
+          sleep-inactive-battery-type = "suspend";
+          power-button-action = "interactive";
+          sleep-inactive-battery-timeout = 1800;
+        };
+        "org/gnome/desktop/session" = {
+          idle-delay = 900;
         };
       };
     };
