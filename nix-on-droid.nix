b95fa3f8fe89235ca9b070f38272ec8244a05367
Nina Morgan <764185+Faeranne@users.noreply.github.com>
2024-03-08 01:11:41 -0500

    Update nix-on-droid.nix

--- nix-on-droid.nix
+++ nix-on-droid.nix
@@ -9,7 +9,6 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   system = {
-    configurationRevision = if self ? rev then self.rev else if self ? dirtyRev then self.dirtyRev else "dirty";
     stateVersion = "23.11"; # Did you read the comment?
   };
   home-manager.config = ./home/droid.nix;
