623de60a08a27a99c52c57c7705e3f8861e8a370
Faer-Anne <nina@projectmakeit.com>
2024-03-10 15:45:59 -0400

    Merge `lib/flake.nix` into root `flake.nix` for more legibility.  Correct description alignment.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-description = "A very nixops flake";
+  description = "A very nixops flake";
 
   inputs = { 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -36,7 +36,35 @@ description = "A very nixops flake";
     };
   };
 
-  outputs = inputs: let
-    lib = import ./lib inputs;
-  in lib.generateFlake;
+  outputs = inputs: with builtins; let
+    inherit (inputs.nixpkgs) lib;
+    flakeLibs = import ./lib inputs;
+    inherit (flakeLibs) mkHost;
+    hostFolders = readDir ./hosts;
+    hosts = foldl' (b: a: let
+      include = if ((getAttr a hostFolders) == "directory") then [a] else [];
+      res = b ++ include;
+    in
+      res
+    ) [] (attrNames hostFolders);
+  in {
+    nixosConfigurations = listToAttrs (map (hostname: let
+      res = mkHost hostname;
+    in {
+      name = hostname;
+      value = lib.nixosSystem res.configuration;
+    }) hosts );
+    agenix-rekey = inputs.agenix-rekey.configure {
+      userFlake = inputs.self;
+      nodes = inputs.self.nixosConfigurations;
+    };
+  } // inputs.flake-utils.lib.eachDefaultSystem (system: rec {
+    pkgs = import inputs.nixpkgs {
+      inherit system;
+      overlays = [ inputs.agenix-rekey.overlays.default ];
+    };
+    devShells.default = pkgs.mkShell {
+      packages = [ pkgs.agenix-rekey ];
+    };
+  });
 }
--- lib/default.nix
+++ lib/default.nix
@@ -2,5 +2,4 @@ inputs: rec
 {
   mkUser = import ./user.nix { inherit inputs; };
   mkHost = import ./host.nix { inherit inputs mkUser; };
-  generateFlake = import ./flake.nix { inherit inputs mkUser mkHost; };
 }
