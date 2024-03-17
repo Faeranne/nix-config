c53d52fb8b046c0dd5ec8fdc1e1490c048c6c7da
Faer-Anne <nina@projectmakeit.com>
2024-03-17 07:10:00 -0400

    local identites for rekeying

--- modules/nixos/security.nix
+++ modules/nixos/security.nix
@@ -12,7 +12,13 @@ in{
       agePlugins = [ pkgs.age-plugin-yubikey ];
       generatedSecretsDir = ../../secrets;
       forceRekeyOnSystem = "x86_64-linux";
-      masterIdentities = [ ../../secrets/identities/yubikey.pub ];
+      masterIdentities = [ "yubikey.pub" ];
+      extraEncryptionPubkeys = [ 
+        "age1yubikey1qtfy343ld8e5sxlvfufa4hh22pm33f6sjq2usx6mmydrmu7txzu7g5xm9vr"
+        "age1yubikey1qdnfvhjlw8j2dkksj9eyxaqwldtqw4427cqjjqxulr5t7gn4flnt25lhuyw"
+        "age1yubikey1qw43gcah5lr95c4klyavduax0drqd5a95lhs8u2wpzqrtcklw5f0uwruyek"
+        "age1yubikey1qwcdxfaalqhntrsrkt7p2nyngdyjc72jr8tehgdzgwwpsl0veflrxncut3x"
+      ];
       hostPubkey = systemConfig.security.pubkey;
     };
   };
