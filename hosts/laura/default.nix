13bfeb5a61af3c538d24f9c54bc2a41eea2107ac
Faer-Anne <nina@projectmakeit.com>
2024-03-18 19:09:52 -0400

    oops... wrong device name

--- hosts/laura/default.nix
+++ hosts/laura/default.nix
@@ -12,18 +12,18 @@
   # the machine-id of this system.
   hostId = "76dc8f17";
   # Primary network interface as reported by `ip addr`
-  netdev = "wpl6s0";
+  netdev = "wpl108s0";
   # Root disk devices for this system.  Prefer `by-path` where possible,
   # but can be `by-id` if the path is not guarenteed, like on cloud servers.
   storage = {
-    root = "/dev/disk/by-id/ata-KINGSTON_SV300S37A120G_50026B733201BED6";
+    root = "/dev/disk/by-id/ata-SAMSUNG_SSD_PM871_M.2_2280_256GB_S208NXAGA31056";
   };
   # Users to add to the system. will build Home-Manager installs for this system too.
   users = [ "nina" ];
   sudo = [ "nina" ];
   # Elements used for security management.
   security = {
-    pubkey = "";
+    pubkey = "age1sn54achmc2y1m6440ftx772d14xxt4ndrr7rfjnq9wj6hgguedysnxfdr2";
   };
   # Service list
   services = [
