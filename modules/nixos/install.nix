8f98c5b2f12644a0b6a995fc24b9608795dc8871
Faer-Anne <nina@projectmakeit.com>
2024-03-21 01:03:19 -0400

    slight cleanup of extraUsers in install

--- modules/nixos/install.nix
+++ modules/nixos/install.nix
@@ -1,8 +1,10 @@
 {pkgs, lib, ...}:let
   keys = import ../../lib/getPubKeys.nix "nina";
 in {
-  users.extraUsers.nixos.openssh.authorizedKeys.keys = keys;
-  users.extraUsers.root.openssh.authorizedKeys.keys = keys;
+  users.extraUsers = {
+    nixos.openssh.authorizedKeys.keys = keys;
+    root.openssh.authorizedKeys.keys = keys;
+  };
   systemd.services.sshd.wantedBy = lib.mkOverride 40 [ "multi-user.target" ];
   services = {
     pcscd.enable = true;
