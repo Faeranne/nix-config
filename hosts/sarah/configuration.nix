c90b5f0bd63f5eb1d83a14fb81d2cd81b248bf6a
Faer-Anne <nina@projectmakeit.com>
2024-04-12 16:38:47 -0400

    add sway value to desktop

--- hosts/sarah/configuration.nix
+++ hosts/sarah/configuration.nix
@@ -1,6 +1,7 @@
 {inputs, pkgs, ...}:{
   virtualisation.waydroid.enable = true;
   environment.systemPackages = with pkgs; [
+    openrgb-with-all-plugins
   ];
   programs.corectrl.enable = true;
 }
--- hosts/sarah/default.nix
+++ hosts/sarah/default.nix
@@ -7,6 +7,7 @@
     "impermanence"
     "amdgpu"
     "gnome"
+    "sway"
     "virtualization"
     "rgb"
     "steam"
