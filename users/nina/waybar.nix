eaabd1d21b2e3cbe00043e920653e95cb7d39c8a
Faer-Anne <nina@projectmakeit.com>
2024-04-14 14:05:53 -0400

    update waybar to be "cooler"

--- users/nina/waybar.nix
+++ users/nina/waybar.nix
@@ -6,40 +6,39 @@
       top_bar = {
         layer = "top";
         position = "top";
-        height = 36;
-        spacing = 4;
+        height = 30;
+        margin = "0 0 0 0";
         modules-left = [
+          "sway/workspaces"
+          "tray"
+          "sway/mode"
         ];
         modules-center = [
-          "clock#time"
-          "custom/separator"
-          "clock#week"
-          "custom/separator_dot"
-          "clock#month"
-          "custom/seperator"
-          "clock#calendar"
+          "clock"
         ];
         modules-right = [
+          "backlight"
+          "pulseaudio"
+          "temperature"
+          "memory"
           "network"
-          "group/misc"
-          "custom/logout_menu"
         ];
-        "clock#time" = {
-          format = "{:%I:%M %p %Ez}";
-        };
-        "clock#week" = {
-          format = "{:%a}";
-        };
-        "clock#month" = {
-          format = "{:%h}";
-        };
-        "custom/separator" = {
-          format = "|";
-        };
-        "custom/separator_dot" = {
-          format = "•";
-        };
-        network = {
+        "sway/workspaces" = {
+          disable-scroll = true;
+          all-outputs = true;
+          format = "{icon}";
+          format-icons = {
+            "1"= "";
+            "2"= "";
+            "3"= "";
+            "4"= "";
+            "5"= "";
+            "6"= "";
+            "7"= "";
+            "8"= "";
+            "9"= "";
+            "10"= "";
+          };
         };
       };
     };
