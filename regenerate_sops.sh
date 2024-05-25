af5b893c861e6aadab531899482c842691549e54
Faer-Anne <nina@projectmakeit.com>
2024-05-25 00:01:21 -0400

    cleanup some unused files

--- regenerate_sops.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for f in secrets/*.pub
-do
-  name=$(echo $(basename $f)|sed 's/\./_/g'| awk '{print toupper($0)}')
-  val=$(cat $f)
-  printf -v "$name" "%s" "$val"
-  export "$name"
-done
-cat templates/.sops.yaml | envsubst > .sops.yaml
