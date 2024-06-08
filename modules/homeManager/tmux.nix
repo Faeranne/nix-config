174b10e1d81b02ed07585de623adc8a56ea76f1b
Faer-Anne <nina@projectmakeit.com>
2024-06-08 09:09:13 -0400

    Fix tmux to support sixel correctly

--- modules/homeManager/tmux.nix
+++ modules/homeManager/tmux.nix
@@ -6,6 +6,11 @@
       keyMode = "vi";
       newSession = true;
       tmuxinator.enable = true;
+      extraConfig = ''
+        set -g allow-passthrough on
+        set -ga update-environment TERM
+        set -ga update-environment TERM_PROGRAM
+      '';
     };
   };
   home.shellAliases = {
