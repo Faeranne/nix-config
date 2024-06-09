cf97cf04f425f23518d695fb91433940e801afb4
Faer-Anne <nina@projectmakeit.com>
2024-06-09 14:55:22 -0400

    add git signing

--- users/nina/git.nix
+++ users/nina/git.nix
@@ -4,6 +4,11 @@
     git = {
       userEmail = "nina@projectmakeit.com";
       userName = "Faer-Anne";
+      extraConfig = {
+        commit.gpgsign = true;
+        gpg.format = "ssh";
+        user.signingkey = "ssh-ed25519 AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJ3irFjnNdb4EuLjzbl3i/kUz6Mcgo1qbI5f3yQ6qJFgAAAACnNzaDpnaXRodWI=";
+      };
     };
   };
   home.packages = with pkgs; [
