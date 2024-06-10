8a7865bd51dbb892d02f2ed44eee0bd8720d2de6
Faer-Anne <nina@projectmakeit.com>
2024-06-09 21:14:24 -0400

    that gitsign feature isn't ready yet... oops

--- users/nina/git.nix
+++ users/nina/git.nix
@@ -5,9 +5,11 @@
       userEmail = "nina@projectmakeit.com";
       userName = "Faer-Anne";
       extraConfig = {
-        commit.gpgsign = true;
-        gpg.format = "ssh";
-        user.signingkey = "ssh-ed25519 AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJ3irFjnNdb4EuLjzbl3i/kUz6Mcgo1qbI5f3yQ6qJFgAAAACnNzaDpnaXRodWI=";
+        # I guess this isn't quite ready to be used here.
+        #commit.gpgsign = true;
+        #gpg.format = "ssh";
+        # Something about needing to use an on-disk file? I need to research this a bit better.
+        #user.signingkey = "ssh-ed25519 ";
       };
     };
   };
