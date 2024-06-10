df956aafbeb158ed43af7fc4727df325a90fe4f5
Faer-Anne <nina@projectmakeit.com>
2024-06-09 21:14:41 -0400

    Add some tmux configs courtesy of dreams-of-code.

--- modules/homeManager/tmux.nix
+++ modules/homeManager/tmux.nix
@@ -5,15 +5,24 @@
       enable = true;
       keyMode = "vi";
       newSession = true;
+      prefix = "C-Space";
       tmuxinator.enable = true;
+      plugins = with pkgs; [
+        tmuxPlugins.sensible
+        tmuxPlugins.vim-tmux-navigator
+      ];
+      mouse = true;
+      baseIndex = 1; 
       extraConfig = ''
         set -g allow-passthrough on
         set -ga update-environment TERM
         set -ga update-environment TERM_PROGRAM
+        bind '"' split-window -v -c "#{pane_current_path}"
+        bind '%' split-window -h -c "#{pane_current_path}"
       '';
     };
   };
   home.shellAliases = {
-    mux = "${pkgs.tmuxinator}";
+    mux = "${pkgs.tmuxinator}/bin/tmuxinator";
   };
 }
