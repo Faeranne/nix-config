1ae122f00d664c839b8358e058a0c0aef493606b
Faer-Anne <nina@projectmakeit.com>
2024-06-27 22:06:13 -0400

    Use presto instead of oh-my-zsh

--- modules/homeManager/zsh.nix
+++ modules/homeManager/zsh.nix
@@ -4,7 +4,23 @@
     zsh = {
       enable = true;
       oh-my-zsh = {
+        enable = false;
+      };
+      prezto = {
         enable = true;
+        editor = {
+          keymap = "vi";
+          dotExpansion = true;
+        };
+        prompt = {
+          theme = "adam";
+        };
+        tmux = {
+          autoStartLocal = true;
+        };
+        pmodules = [
+          "tmux"
+        ];
       };
     };
   };
