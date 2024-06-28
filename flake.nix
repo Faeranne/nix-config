942bde957ebf64f2bf819bb8d34ea79dab1e64ee
Faer-Anne <nina@projectmakeit.com>
2024-06-27 22:06:28 -0400

    remove subsituters in nixConfig for now.

--- flake.nix
+++ flake.nix
@@ -258,7 +258,7 @@
     });
   };
   nixConfig = {
-    extra-substituters = [ "https://yazi.cachix.org" ];
-    extra-trusted-public-keys = [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
+#   extra-substituters = [  ];
+#   extra-trusted-public-keys = [  ];
   };
 }
