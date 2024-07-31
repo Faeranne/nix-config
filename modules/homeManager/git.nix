09501c7db30ae7289d44b3ad0b499f691ff8fedd
Faer-Anne <nina@projectmakeit.com>
2024-07-31 17:44:46 -0400

    make git support everything needed

--- modules/homeManager/git.nix
+++ modules/homeManager/git.nix
@@ -6,6 +6,7 @@ in
   programs = {
     git = {
       enable = true;
+      package = pkgs.gitFull;
       aliases = lib.mkDefault {
         lg = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
       };
