3e9a73823fc8b59bc7e444410c05dddcb3810ed1
Faer-Anne <nina@projectmakeit.com>
2024-08-05 20:13:57 -0400

    try function over template

---
+++ modules/containers/containerBase.nix
@@ -0,0 +1,41 @@
+name: {self, config, ...}:{
+  age.secrets = {
+    "wg${name}" = {
+      rekeyFile = self + "/secrets/containers/${name}/wireguard.age";
+      group = "systemd-network";
+      mode = "770";
+      generator = {
+        script = "wireguard";
+        tags = [ "wireguard" ];
+      };
+    };
+
+  };
+  systemd.network = {
+    networks."wg${name}" = {
+      matchConfig.name = "wg${name}";
+      networkConfig = {
+        IPForward = true;
+      };
+    };
+    netdevs."wg${name}" = {
+      enable = true;
+      netdevConfig = {
+        Kind = "wireguard";
+        Name = "wg${name}";
+      };
+      wireguardConfig = {
+        PrivateKeyFile = config.age.secrets."wg${name}".path;
+      };
+    };
+  };
+  containers.${name} = {
+    privateNetwork = true;
+    restartIfChanged = true;
+    autoStart = true;
+    hostBridge = "brCont";
+    interfaces = [
+      "wg${name}"
+    ];
+  };
+}
--- modules/containers/grocy.nix
+++ modules/containers/grocy.nix
@@ -1,47 +1,19 @@
 {self, config, ...}:{
   imports = [
-    ./containerDefault.nix
+    (import ./containerBase.nix "grocy")
   ];
-  age.secrets = {
-    wggrocy = {
-      rekeyFile = self + "/secrets/containers/grocy/wireguard.age";
-      group = "systemd-network";
-      mode = "770";
-      generator = {
-        script = "wireguard";
-        tags = [ "wireguard" ];
-      };
-    };
-
-  };
   systemd.network = {
     networks.wggrocy = {
-      matchConfig.name = "wggrocy";
       address = [ "10.100.1.2/16" ];
-      networkConfig = {
-        IPForward = true;
-      };
     };
     netdevs.wggrocy = {
-      netdevConfig = {
-        Kind = "wireguard";
-        Name = "wggrocy";
-      };
       wireguardConfig = {
-        PrivateKeyFile = config.age.secrets.wggrocy.path;
         ListenPort = 51821;
       };
     };
   };
   containers.grocy = {
-    privateNetwork = true;
-    restartIfChanged = true;
-    autoStart = true;
     localAddress = "/16";
-    hostBridge = "brCont";
-    interfaces = [
-      "wggrocy"
-    ];
     bindMounts = {
       "/var/lib/grocy" = {
         hostPath = "/Storage/volumes/grocy";
