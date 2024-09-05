ff88a50ec5817f982fe6c4e7c40989b0fc2e0a38
Faer-Anne <nina@projectmakeit.com>
2024-09-05 07:20:58 -0400

    add required nginx and postgres configs to container. set ownership for volumes. set startup timeout

--- modules/containers/netbox.nix
+++ modules/containers/netbox.nix
@@ -28,10 +28,12 @@ in {
       "/var/lib/netbox" = {
         isReadOnly = false;
         create = true;
+        owner = "container:container";
       };
       "/var/lib/postgres" = {
         isReadOnly = false;
         create = true;
+        owner = "postgres:postgres";
       };
       "/run/secrets/netbox" = {
         isReadOnly = true;
@@ -42,6 +44,9 @@ in {
     specialArgs = {
       port = 8096;
     };
+
+    # Initial startup from a fresh container can take an extended amount of time
+    timeoutStartSec = "5min";
 
     config = let
       hostConfig = config;
@@ -54,12 +59,40 @@ in {
           allowedTCPPorts = [ port ];
         };
       };
-      services.netbox = {
-        inherit port;
-        enable = true;
-        package = pkgs.netbox;
-        secretKeyFile = "/run/secrets/netbox";
-        listenAddress = lib.removeSuffix "/32" (lib.elemAt hostConfig.networking.wireguard.interfaces."wg${containerName}".ips 0);
+      services = {
+        postgresql = {
+          dataDir = "/var/lib/postgres";
+          package = pkgs.postgresql_15;
+        };
+        netbox = {
+          enable = true;
+          port = 8001;
+          package = pkgs.netbox;
+          secretKeyFile = "/run/secrets/netbox";
+          listenAddress = "127.0.0.1";
+          settings = {
+            ALLOWED_HOSTS = [ "netbox.faeranne.com" ];
+            CSRF_TRUSTED_ORIGINS = [ "https://netbox.faeranne.com" ];
+          };
+        };
+        nginx = {
+          enable = true;
+          user = "netbox"; # otherwise nginx cant access netbox files
+          recommendedProxySettings = true; # otherwise you will get CSRF error while login
+          defaultListen = [{
+           addr = (lib.removeSuffix "/32" (lib.elemAt hostConfig.networking.wireguard.interfaces."wg${containerName}".ips 0));
+           port = port;
+          }];
+          virtualHosts.default = {
+            default = true;
+            locations = {
+              "/" = {
+                proxyPass = "http://127.0.0.1:8001";
+              };
+              "/static/" = { alias = "${config.services.netbox.dataDir}/static/"; };
+            };
+          };
+        };
       };
       users.users.netbox.uid = hostConfig.users.users.container.uid;
       users.groups.netbox.gid = hostConfig.users.groups.container.gid;
