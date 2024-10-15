49433745bf83bfc5043e166be293c5a303458190
Faer-Anne <nina@projectmakeit.com>
2024-10-15 19:50:42 -0400

    add firewall settings

--- hosts/hazel/networking.nix
+++ hosts/hazel/networking.nix
@@ -2,6 +2,12 @@
   boot.kernel.sysctl = {
     "net.ipv6.conf.all.disable_ipv6" = 1;
   };
+  networking = {
+    firewall = {
+      allowedTCPPorts = [ 25565 3876 ];
+      allowedUDPPorts = [ 24454 ];
+    };
+  };
   systemd.network.networks = {
     "eno1" = {
       enable = true;
