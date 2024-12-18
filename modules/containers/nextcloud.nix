789f3315da93752b358194331bd19d1c12d2ca8e
Faer-Anne <nina@projectmakeit.com>
2024-12-18 06:32:34 -0500

    update nextcloud

--- modules/containers/nextcloud.nix
+++ modules/containers/nextcloud.nix
@@ -66,21 +66,22 @@ in {
           nextcloud = {
             inherit hostName;
             enable = true;
-            package = pkgs.nextcloud29;
+            package = pkgs.nextcloud30;
             https = true;
             configureRedis = true;
             extraApps = {
-              inherit (config.services.nextcloud.package.packages.apps) bookmarks calendar contacts cookbook cospend deck forms polls phonetrack memories maps notify_push richdocuments spreed tasks twofactor_nextcloud_notification twofactor_webauthn;
+              inherit (config.services.nextcloud.package.packages.apps) bookmarks calendar contacts cookbook cospend deck forms phonetrack memories maps notify_push richdocuments spreed tasks twofactor_webauthn;
+
               pride_flags = pkgs.fetchNextcloudApp {
-                url = "https://git.finally.coffee/finallycoffee/nextcloud-pride-flags/releases/download/1.1.0/pride_flags-1.1.0.tar.gz";
-                sha256 = "sha256-vFdabHrdBqpS4I81cthtyoy7bRC7S/rDC5ixrwtkR/c=";
+                url = "https://git.finally.coffee/finallycoffee/nextcloud-pride-flags/releases/download/1.1.1/pride_flags-1.1.1.tar.gz";
+                sha256 = "sha256-AC1DsCHzKVhXtYStSRBDFGgRHlK4WUegAJ5m5mr02yo=";
                 license = "gpl3";
               };
-              integration_paperless = pkgs.fetchNextcloudApp {
+              /*integration_paperless = pkgs.fetchNextcloudApp {
                 url = "https://github.com/nextcloud-releases/integration_paperless/releases/download/v1.0.3/integration_paperless-v1.0.3.tar.gz";
                 sha256 = "sha256-ARjs8cCUJICJaZiMIIt/lYk15WlXzzRqAQBWwax6HY4=";
                 license = "agpl3Only";
-              };
+              };*/
               files_archive = pkgs.fetchNextcloudApp {
                 url = "https://github.com/rotdrop/nextcloud-app-files-archive/releases/download/v1.2.3/files_archive.tar.gz";
                 sha256 = "sha256-D4D8OXlgF0Gv3NRSuq69Quu9jN96Blk0+QwiF1X63EU=";
@@ -91,19 +92,19 @@ in {
                 sha256 = "sha256-AuRZcyJcjlaGt/83+0BHqyN7tZwIvxQDUF3JqzSesk8=";
                 license = "agpl3Only";
               };
-              secrets = pkgs.fetchNextcloudApp {
+              /*secrets = pkgs.fetchNextcloudApp {
                 url = "https://github.com/theCalcaholic/nextcloud-secrets/releases/download/v2.0.3/secrets.tar.gz";
                 sha256 = "sha256-6Q3j40EGENced4bKFAh1F63RVTk3gGHOmmaIX2ERj5c=";
                 license = "agpl3Only";
-              };
+              };*/
               guests = pkgs.fetchNextcloudApp {
-                url = "https://github.com/nextcloud-releases/guests/releases/download/v3.1.0/guests-v3.1.0.tar.gz";
-                sha256 = "sha256-3AuW/1fgINxLk5HXJ4Qy3rUB4G6IPa6TDsHG5PfzTiM=";
+                url = "https://github.com/nextcloud-releases/guests/releases/download/v4.0.1/guests-v4.0.1.tar.gz";
+                sha256 = "sha256-rd1pVrlk2xZBxy4HZ0egew8SLAhcln/gSZ2Er5/FKtw=";
                 license = "agpl3Only";
               };
               duplicatefinder = pkgs.fetchNextcloudApp {
-                url = "https://github.com/eldertek/duplicatefinder/releases/download/v1.2.5/duplicatefinder-v1.2.5.tar.gz";
-                sha256 = "sha256-gMrpbgXCZnKKWf1bEir71Ojd7N98i8xU/jg0N/DXKWM=";
+                url = "https://github.com/eldertek/duplicatefinder/releases/download/v1.2.9/duplicatefinder-v1.2.9.tar.gz";
+                sha256 = "sha256-xDLDUGhjvGDMukXxhirocnDRnDu/R8BUOLUGquYbv1U=";
                 license = "agpl3Only";
               };
               recognize = pkgs.fetchNextcloudApp {
