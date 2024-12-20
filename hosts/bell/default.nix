0d94864fad5b28c800ab90b7d156ae8b40451ca7
Faer-Anne <nina@projectmakeit.com>
2024-12-20 05:13:45 -0500

    add bell

---
+++ hosts/bell/default.nix
@@ -0,0 +1,37 @@
+{
+  self,
+  ...
+}: let
+  localCfg = builtins.fromJSON (builtins.readFile ./config.json);
+in {
+  imports = with self.nixosModules; [
+    base
+    extras.storage
+    hardware.cpu.intel
+    self.userModules.nina
+  ];
+
+  networking = {
+    hostName = "bell";
+    hostId = "1cd0fa6c";
+    firewall = {
+      allowedTCPPorts = [];
+      allowedUDPPorts = [];
+    };
+  };
+
+  environment.systemPackages = [
+  ];
+
+  age.rekey.hostPubkey = "${localCfg.pubkey}";
+
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-uuid/${localCfg.bootID}";
+      fsType = "vfat";
+      options = ["fmask=0022" "dmask=0022"];
+    };
+  };
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+}
