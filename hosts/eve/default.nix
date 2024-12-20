4ff3d6be067f42f87812075710f2356881fe46c4
Faer-Anne <nina@projectmakeit.com>
2024-12-20 07:02:12 -0500

    update eve cause stuff was missed

--- hosts/eve/default.nix
+++ hosts/eve/default.nix
@@ -1,10 +1,12 @@
 {
   self,
-  pkgs,
   ...
-}: {
+}: let
+  localCfg = builtins.fromJSON (builtins.readFile ./config.json);
+in {
   imports = with self.nixosModules; [
     base
+    server
     extras.storage
     hardware.cpu.intel
     self.userModules.nina
@@ -21,6 +23,16 @@
 
   environment.systemPackages = [
   ];
+
+  age.rekey.hostPubkey = "${localCfg.pubkey}";
+
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-uuid/${localCfg.bootID}";
+      fsType = "vfat";
+      options = ["fmask=0022" "dmask=0022"];
+    };
+  };
 
   nixpkgs.hostPlatform = "x86_64-linux";
 }
