e404cab058786b2e896d7ee9b4f8b272815e758c
Faer-Anne <nina@projectmakeit.com>
2025-01-06 02:55:45 -0500

    add global optino to nexos

--- modules/nexos/default.nix
+++ modules/nexos/default.nix
@@ -11,20 +11,27 @@ self: {
       lib,
       ...
     }:let
-      inherit (lib) mkEnableOption;
+      inherit (lib) mkOption mkEnableOption;
+      inherit (lib.types) attrs;
     in {
       options = {
         nexos = {
           enable = mkEnableOption "Enable all NexOS defaults and features.";
+          global = mkOption {
+            description = "Global option entrypoint";
+            type = attrs;
+            default = {enable = false;};
+          };
         };
       };
 
       imports = [
         ./hardware
+        ./info
         ./networking.nix
+        ./nixconfig.nix
         ./secrets.nix
         ./storage.nix
-        ./info
       ];
     };
   };
