76e7cfb44473a80808a241e0a96d820e4e26d0d2
Faer-Anne <nina@projectmakeit.com>
2025-01-06 02:57:00 -0500

    add nix config settings

--- modules/nexos/nixconfig.nix
+++ modules/nexos/nixconfig.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   config,
   lib,
   ...
@@ -6,7 +7,8 @@
   inherit (lib) mkIf mkOption mkEnableOption;
   inherit (lib.types) str;
   enable = config.nexos.enable;
-  cfg = config.nexos.networking;
+  cfg = config.nexos.nixpkgs;
+  secrets = config.nexos.security.secretsFolder;
   configFile = if config.nexos.info.configPath != null then builtins.fromJSON (builtins.readFile config.nexos.info.configPath) else null;
 in {
 
@@ -14,10 +16,86 @@ in {
     nexos = {
       nixpkgs = {
         enableNur = mkEnableOption "Enable NUR configs";
+        repo = {
+          base = mkOption {
+            type = str;
+            default = "git+https://git.faeranne.com/faeranne/nix-config";
+            description = "Base repo for this set of systems.";
+          };
+          tag = mkOption {
+            type = str;
+            default = "rebuild2";
+            description = "tag to build from.";
+          };
+        };
       };
     };
   };
 
   config = mkIf enable {
+    system = {
+      autoUpgrade = mkIf config.nexos.networking.online {
+        operation = "switch";
+        flake = "${cfg.repo.base}?ref=${cfg.repo.tag}";
+      };
+    };
+    nix = {
+      settings = {
+        substituters = [
+          "https://nix-community.cachix.org"
+          "https://cache.nixos.org"
+          "https://ncache.faeranne.com"
+        ];
+        trusted-public-keys = [
+          "ncache.faeranne.com:f0zP4VrDZbT9A/Xx3tfLD9M9sI9maSvFJg3zbGh7Ty0=%"
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+        # enables some commonly used experimental features
+        # nix-command is the actual `nix` command, since that was introduced
+        #   with flakes
+        # flakes are the format I use for managing this system. specifically
+        #   it enables things like `nix run` while using the `flake.nix` file
+        #   in the root of this repo
+        # ca-derivations is a new way to address derivations by what they contain
+        #   instead of their build inputs.  In theory this will reduce rebuilding
+        #   of some derivations by detecting that they will result in the same
+        #   content, and just reusing the same derivation address.
+        #   used in some nur stuff.
+        experimental-features = ["nix-command" "flakes" "ca-derivations"];
+      };
+      nixPath = [
+        "nixpkgs=${inputs.nixpkgs}"
+      ];
+      # I was trying to setup github-tokens to allow additional nix flake update calls
+      # but it didn't work right due to agenix not being correctly supported here.
+      # TODO: readd when scalple is working
+      extraOptions = ''
+        connect-timeout = 5
+      '';
+      # this controls the store garbage collection.  Mostly used to ensure
+      # /nix/store doesn't ballon from being used a bunch and holding
+      # really old nixos builds.
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 7d";
+      };
+      registry = {
+        nixpkgs.flake = inputs.nixpkgs;
+      };
+    };
+
+    # see modules/nixos/base/security.nix for more details about this
+    age = {
+      secrets = {
+        # this is the access tokens intended to be used with the above extraOptions field
+        # its contents are manually managed since I don't get to make the keys themselves
+        flake-accessTokens = {
+          rekeyFile = secrets + "/accessTokens.age";
+          mode = "770";
+          group = "nixbld";
+        };
+      };
+    };
   };
 }
