c29e64c0ca4bc2105fb5bc375b9b79bc5ec09b0d
Faer-Anne <nina@projectmakeit.com>
2025-01-18 13:47:41 -0500

    merge nfs and udisk2

--- hosts/bell/default.nix
+++ hosts/bell/default.nix
@@ -22,12 +22,17 @@ in {
     };
   };
 
-  services.nfs.server = {
-    enable = true;
-    exports = ''
-      /export   192.168.1.*(rw,fsid=0,no_subtree_check)
-      /export/nixstore   192.168.1.*(ro,nohide=0,insecure,no_subtree_check)
-    '';
+  services = {
+    nfs.server = {
+      enable = true;
+      exports = ''
+        /export   192.168.1.*(rw,fsid=0,no_subtree_check)
+        /export/nixstore   192.168.1.*(ro,nohide=0,insecure,no_subtree_check)
+      '';
+    };
+    udisks2 = {
+      enable = true;
+    };
   };
 
   boot = {
