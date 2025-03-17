e752886e1d6072c17bc41ebd25c59fba6deb7034
Faer-Anne <nina@projectmakeit.com>
2025-03-17 12:50:17 -0400

    Virtualbox keeps breaking things, disabling

--- modules/nixos/emulation/default.nix
+++ modules/nixos/emulation/default.nix
@@ -1,11 +1,11 @@
 {...}: {
   programs.virt-manager.enable = true;
   virtualisation = {
-    virtualbox.host = {
+    /*virtualbox.host = {
       enable = true;
       enableExtensionPack = true;
       enableHardening = true;
-    };
+    };*/
     libvirtd.enable = true;
   };
 }
